d2dd12772b2cd72e146f42ff9bcfb213b96756ae
Erik Bremen <strtok@strtok.net>
2016-12-29 15:33:51 -0800

    Some ROM decoding

--- rom.js
+++ rom.js
@@ -1,9 +1,37 @@
+/*
+     NES 2.0 Rom Format:
+     http://wiki.nesdev.com/w/index.php/NES_2.0
+ */
+var assert = require("assert");
+var _ = require("underscore");
 
 class Rom {
-    foo() {
-        return 42;
+    constructor(uint8Array) {
+        this.data = uint8Array;
+
+        // bytes 0-3 must be "NES<0x1A>"
+        const header = new Uint8Array([78, 69, 83, 0x1A]);
+        if (!_.isEqual(this.data.slice(0,4), header)) {
+            throw "NES header missing";
+        }
+
+        // byte 4 is the number of 16k ROM pages
+        this.romPages16 = this.data[4];
+
+        // byte 5 is the number of 8k ROM pages (0 indicates CHR RAM)
+        this.romPages8 = this.data[5];
+
+        // byte 6 and 7 contain various flags
+        // 7       0
+        // ---------
+        // NNNN FTBM
+        //
+        // N: Lower 4 bits of the mapper number
+        // F: Four screen mode. 0 = no, 1 = yes. (When set, the M bit has no effect)
+        // T: Trainer.  0 = no trainer present, 1 = 512 byte trainer at 7000-71FFh
+        // B: SRAM at 6000-7FFFh battery backed.  0= no, 1 = yes
+        // M: Mirroring.  0 = horizontal, 1 = vertical.
     }
 }
-
 
 module.exports.Rom = Rom
--- test/rom_test.js
+++ test/rom_test.js
@@ -2,11 +2,16 @@ var assert = require('assert');
 var rom = require('../rom')
 var nestest = require('./nestest_rom');
 
+
 describe('rom', () => {
     describe('load', () => {
-        it('xxx', function() {
-            var romData = nestest.byteArray();
-            romData.toString();
+        it('constructs from nestest', function() {
+            var romBuffer = nestest.byteArray();
+            var r = new rom.Rom(romBuffer);
+            assert.equal(r.romPages16, 1);
+            assert.equal(r.romPages8, 1);
+
         });
     });
+
 });
