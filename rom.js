96e1bac953bd16ff773cf867dbc928cf28cc94a1
Erik Bremen <strtok@strtok.net>
2016-12-29 16:03:24 -0800

    Finish rom header

--- rom.js
+++ rom.js
@@ -2,8 +2,10 @@
      NES 2.0 Rom Format:
      http://wiki.nesdev.com/w/index.php/NES_2.0
  */
+
 var assert = require("assert");
 var _ = require("underscore");
+var debug = require("debug")("nes:rom")
 
 class Rom {
     constructor(uint8Array) {
@@ -16,12 +18,12 @@ class Rom {
         }
 
         // byte 4 is the number of 16k ROM pages
-        this.romPages16 = this.data[4];
+        this.prgRomSize = this.data[4];
 
         // byte 5 is the number of 8k ROM pages (0 indicates CHR RAM)
-        this.romPages8 = this.data[5];
+        this.chrRomSize = this.data[5];
 
-        // byte 6 and 7 contain various flags
+        // byte 6 flags
         // 7       0
         // ---------
         // NNNN FTBM
@@ -31,7 +33,30 @@ class Rom {
         // T: Trainer.  0 = no trainer present, 1 = 512 byte trainer at 7000-71FFh
         // B: SRAM at 6000-7FFFh battery backed.  0= no, 1 = yes
         // M: Mirroring.  0 = horizontal, 1 = vertical.
+        var flag6 = this.data[6];
+        this.fourScreenMode = !!(flag6 & 0b00001000);
+        this.trainer = !!(flag6 & 0b00000100);
+        this.SRAM = !!(flag6 & 0b00000010);
+        this.mirroringVertical = !!(flag6 & 0b00000001);
+
+        // byte 7 flags
+        // 7       0
+        // ---------
+        // NNNN xxPV
+        //
+        // N: Upper 4 bits of the mapper number
+        // P: Playchoice 10.  When set, this is a PC-10 game
+        // V: Vs. Unisystem.  When set, this is a Vs. game
+        // x: these bits are not used in iNES.
+        var flag7 = this.data[7];
+        this.playChoice10 = !!(flag7 & 0b00000010);
+        this.vsGame = !!(flag7 & 0b00000001);
+
+        //byte 8 indicates size of PRG RAM in 8KB units
+        this.prgRamSize = this.data[8]
     }
+
+    get mirroringHorizontal () { return !this.mirroringVertical; }
 }
 
 module.exports.Rom = Rom
--- test/rom_test.js
+++ test/rom_test.js
@@ -5,12 +5,22 @@ var nestest = require('./nestest_rom');
 
 describe('rom', () => {
     describe('load', () => {
-        it('constructs from nestest', function() {
+        it('constructs from nestest.nes', function() {
             var romBuffer = nestest.byteArray();
             var r = new rom.Rom(romBuffer);
-            assert.equal(r.romPages16, 1);
-            assert.equal(r.romPages8, 1);
+            assert.equal(r.prgRomSize, 1);
+            assert.equal(r.chrRomSize, 1);
 
+            assert(!r.trainer);
+            assert(!r.mirroringVertical);
+            assert(r.mirroringHorizontal);
+            assert(!r.SRAM);
+            assert(!r.trainer);
+
+            assert(!r.playChoice10);
+            assert(!r.vsGame);
+
+            assert.equal(r.prgRamSize, 0);
         });
     });
 
