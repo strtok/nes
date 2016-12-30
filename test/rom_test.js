bc7c3e9533bc7078b05d1688637a382ecd739b4b
Erik Bremen <strtok@strtok.net>
2016-12-29 17:56:57 -0800

    Fix test hierarchy

--- test/rom_test.js
+++ test/rom_test.js
@@ -4,30 +4,27 @@ var nestest = require('./nestest_rom');
 
 
 describe('rom', () => {
-    describe('load', () => {
-        it('constructs from nestest.nes', function() {
-            var romBuffer = nestest.byteArray();
-            var r = new rom.Rom(romBuffer);
+    it('constructs from nestest.nes', function() {
+        var romBuffer = nestest.byteArray();
+        var r = new rom.Rom(romBuffer);
 
-            assert.equal(r.prgRomSize, 1);
-            assert.equal(r.prgRom.length, 1);
-            assert.equal(r.prgRom[0].length, 16*1024);
+        assert.equal(r.prgRomSize, 1);
+        assert.equal(r.prgRom.length, 1);
+        assert.equal(r.prgRom[0].length, 16*1024);
 
-            assert.equal(r.chrRomSize, 1);
-            assert.equal(r.chrRom.length, 1);
-            assert.equal(r.chrRom[0].length, 8*1024);
+        assert.equal(r.chrRomSize, 1);
+        assert.equal(r.chrRom.length, 1);
+        assert.equal(r.chrRom[0].length, 8*1024);
 
-            assert(!r.hasTrainer);
-            assert(!r.mirroringVertical);
-            assert(r.mirroringHorizontal);
-            assert(!r.SRAM);
-            assert(!r.hasTrainer);
+        assert(!r.hasTrainer);
+        assert(!r.mirroringVertical);
+        assert(r.mirroringHorizontal);
+        assert(!r.SRAM);
+        assert(!r.hasTrainer);
 
-            assert(!r.playChoice10);
-            assert(!r.vsGame);
+        assert(!r.playChoice10);
+        assert(!r.vsGame);
 
-            assert.equal(r.prgRamSize, 0);
-        });
+        assert.equal(r.prgRamSize, 0);
     });
-
 });
