efd8eb17212b1b2140379718a852c2743e82ceba
Erik Bremen <strtok@strtok.net>
2016-12-31 13:44:20 -0800

    Add registers

--- cpu.js
+++ cpu.js
@@ -3,10 +3,18 @@ const debug = require('debug')('nes:cpu');
 class CPU {
     constructor(memory) {
         this.memory = memory;
+        this.register = {
+            p: 0x24,      // Processor status flags
+            pc: 0xC000,   // Program Counter
+            a: 0x00,      // Accumulator
+            sp: 0xFD,     // Stack Pointer
+            x: 0x00,      // X (general purpose)
+            y: 0x00       // Y (general purpose)
+        }
     }
 
     execute() {
-        throw new Error('unknown opcode');
+        throw new Error('invalid op-code');
     }
 };
 
--- test/nestest_test.js
+++ test/nestest_test.js
@@ -2,6 +2,7 @@ let assert = require('chai').assert;
 let debug = require('debug')('nes:nestest');
 let Rom = require('../rom').Rom;
 let Memory = require('../memory').Memory;
+let CPU = require('../cpu').CPU;
 let nestest = require('./nestest_rom');
 let log = require('./nestest_log').log;
 
@@ -9,5 +10,9 @@ describe('nestest', () => {
     it('executes as expected', function() {
         let rom = new Rom(nestest.byteArray());
         let memory = new Memory(rom.prgRom);
+        let cpu = new CPU(memory);
+        while(true) {
+            cpu.execute();
+        }
     });
 });
